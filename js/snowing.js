be008d7eab58414521a88d1099464a3c86bfef37
Katherine <krosenk729@gmail.com>
2018-02-04 07:17:20 -0500

    cleaned up styles and got canvas to do its thing

--- js/snowing.js
+++ js/snowing.js
@@ -1,52 +1,65 @@
-var canvas = document.querySelector('canvas');
-var ctx = canvas.getContext('2d');
+const canvas = document.querySelector('canvas'), 
+	ctx = canvas.getContext('2d'),
+	MAXW = 2024,
+	MAXH = 2400;
 
-var width = canvas.width = window.innerWidth;
-var height = canvas.height = window.innerHeight;
+let width = canvas.width = $('body').width();
+let height = canvas.height = $('body').height();
+$(window).on('resize', function(){
+	console.log('resize');
+	width = canvas.width = $('body').width();
+	height = canvas.height = $('body').height();
+});
 
-var tinySnowBalls = [];
-var teenySnowBalls = [];
-
-
+let tinySnowBalls = [];
+let teenySnowBalls = [];
 function SnowBall(x, y, r, vel){
 	this.x = x;
 	this.y = y;
 	this.r = r;
-	this.vel = vel || 3000;
+	this.vel = vel || 1;
 }
 
 SnowBall.prototype.draw = function(){
-	ctx.fillStyle = 'rgba(0,0,0,1)'; //update
+	ctx.fillStyle = 'rgba(180, 231, 248, .3)';
 	ctx.beginPath();
 	ctx.arc(this.x, this.y, this.r, 0, 2*Math.PI);
 	ctx.fill();
-
 }
 SnowBall.prototype.move = function(){
 	this.y += this.vel;
+	if( this.y > MAXH + 10 ){ this.y = -10 ; }
+}
+
+function makeSnow(){
+	for(let y = 12 ; y < MAXH ; y += 48){
+		let ittTiny = 12;
+		let ittTeeny = 20;
+
+		while(ittTeeny < MAXW){
+			let snowFlake = new SnowBall(ittTeeny, y , 8);
+			tinySnowBalls.push(snowFlake);
+			ittTeeny += 32;
+		}
+
+		while(ittTiny < MAXW){
+			let snowFlake1 = new SnowBall(ittTiny, y+32, 4),
+				snowFlake2 = new SnowBall(ittTiny+8, y+24, 4),
+				snowFlake3 = new SnowBall(ittTiny+16, y+32, 4),
+				snowFlake4 = new SnowBall(ittTiny+40, y+16, 4),
+				snowFlake5 = new SnowBall(ittTiny+48, y+24, 4),
+				snowFlake6 = new SnowBall(ittTiny+56, y+16, 4);
+			teenySnowBalls = [...teenySnowBalls, snowFlake1, snowFlake2, snowFlake3, snowFlake4, snowFlake5, snowFlake6];
+			ittTiny += 80;
+		}
+	}
+	console.log('teeny', teenySnowBalls, 'tiny', tinySnowBalls);
 }
 
 function makeItSnow(){
-	let countTiny = Math.floor( width / 32 ) + 1;
-	let countTeeny = Math.floor( width / 80 ) + 3;
-	let ittTiny = 12;
-	let ittTeeny = 20;
-
-	while(tinySnowBalls.length < countTiny){
-		let snowFlake = new SnowBall(ittTeeny, 12, 8);
-		tinySnowBalls.push(snowFlake);
-		ittTeeny += 32;
-	}
-	while(teenySnowBalls.length < countTeeny){
-		let snowFlake1 = new SnowBall(ittTiny, 44, 4),
-			snowFlake2 = new SnowBall(ittTiny+8, 36, 4),
-			snowFlake3 = new SnowBall(ittTiny+16, 44, 4),
-			snowFlake4 = new SnowBall(ittTiny+40, 28, 4),
-			snowFlake5 = new SnowBall(ittTiny+48, 36, 4),
-			snowFlake6 = new SnowBall(ittTiny+56, 28, 4);
-		teenySnowBalls = [...teenySnowBalls, snowFlake1, snowFlake2, snowFlake3, snowFlake4, snowFlake5, snowFlake6];
-		ittTeeny += 80;
-	}
+	// get rid of old snow
+	ctx.fillStyle = '#fff';
+	ctx.fillRect(0, 0, width, height);
 
 	teenySnowBalls.forEach(function(item){
 		item.draw();
@@ -57,19 +70,8 @@ function makeItSnow(){
 		item.move();
 	});
 
+	requestAnimationFrame(makeItSnow);
 }
-//setInterval(makeItSnow, 3000);
-
-
-
-
-// first circle 
-// x = 12, y = 12, r = 8
-
-// next row
-// x = 20, y = 44, r = 4
-// x = 28, y = 36, r = 4 (x = firstx + 8)
-// x = 36, y = 44, r = 4 (x = firstx + 16)
-// x = 60, y = 28, r = 4 (x = firstx + 40)
-// x = 68, y = 36, r = 4 (x = firstx + 48) 
-// x = 76, y = 28, r = 4 (x = firstx + 56)
+makeSnow();
+makeItSnow();
+//setInterval(makeItSnow, 100);
